cc887e1ffce4fe931c876eefc3f162c060807754
YuliyaMinsk <yuliya.rostova@gmail.com>
2023-05-09 12:20:18 +0600

    feat: add examples of env files

--- src/shared/utils/create-path.ts
+++ src/shared/utils/create-path.ts
@@ -1,6 +1,7 @@
 function createPath(path: string): string {
-  console.log('createPath', `${import.meta.env.VITE_APP_BASE_URL}${path}`);
-  console.log('createPath', import.meta.env);
+  // TODO: delete after debug
+  // console.log('createPath', `${import.meta.env.VITE_APP_BASE_URL}${path}`);
+  // console.log('createPath', import.meta.env);
   return `${import.meta.env.VITE_APP_BASE_URL}${path}`;
 }
 
