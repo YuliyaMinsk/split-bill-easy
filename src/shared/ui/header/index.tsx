942cca6d7bfda71854796a039e10353ab3e17fb3
YuliyaMinsk <yuliya.rostova@gmail.com>
2023-05-28 12:51:52 +0600

    SBE-3: add button to header

--- src/shared/ui/header/index.tsx
+++ src/shared/ui/header/index.tsx
@@ -1,4 +1,5 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import { Menu } from '@mui/icons-material';
 import { useTranslation } from 'react-i18next';
 
 type HeaderProps = {
@@ -10,7 +11,10 @@ const Header = ({ text }: HeaderProps): JSX.Element => {
 
   return (
     <AppBar position="sticky">
-      <Toolbar sx={{ justifyContent: 'center' }}>
+      <Toolbar>
+        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <Menu />
+        </IconButton>
         <Typography variant="h6" component="h1">
           {text || t('Split Bill Easy')}
         </Typography>
