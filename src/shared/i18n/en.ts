b0dcb195083b8b69b5e7443cd009efdf20e4cfe4
YuliyaMinsk <yuliya.rostova@gmail.com>
2023-12-18 17:51:11 +0600

    SBE-17: split language file

---
+++ src/shared/i18n/en.ts
@@ -0,0 +1,71 @@
+const enTranslations = {
+  translation: {
+    'Split Bill Easy': 'Split Bill Easy',
+    'Welcome to React': 'Welcome to React and react-i18next',
+
+    // top navigation
+    'Choose language': 'Choose language',
+    'English': 'English',
+    'Russian': 'Russian',
+    'Profile': 'Profile',
+
+    // bottom navigation
+    'Payers': 'Payers',
+    'Items': 'Items',
+    'Calculations': 'Calculations',
+
+    // payers
+    'Name': 'Name',
+    'New name': 'New name',
+    'Enter a new name': 'Enter a new name',
+
+    // items
+    'There are no items in your bill. Fill the form below to add it':
+      'There are no items in your bill. Fill the form below to add it',
+    'Item': 'Item',
+    'Enter an item': 'Enter an item',
+    'Item cost': 'Item cost',
+    'Enter an item cost': 'Enter an item cost',
+    'Quantity': 'Quantity',
+    'Enter a quantity': 'Enter a quantity',
+    'Choose who bought this item': 'Choose who bought this item',
+    'Save': 'Save',
+    'Edit': 'Edit',
+    'Delete': 'Delete',
+    'Calculate after extra charges': 'Calculate after extra charges',
+    'Copy to clipboard for everyone': 'Copy to clipboard for everyone',
+    'Copy to clipboard for ': 'Copy to clipboard for ',
+
+    // copy to clipboard
+    'Bill for': 'Bill for',
+    'Total price': 'Total price',
+
+    // services
+    'Service Charge': 'Service Charge',
+    'Tips': 'Tips',
+    'Discount': 'Discount',
+    'Tax': 'Tax',
+
+    // error messages
+    'Error: Incorrect amount': 'Error: Incorrect amount',
+    'Item is required': 'Item is required',
+    'Price is required': 'Price is required',
+    'Quantity is required': 'Quantity is required',
+
+    // main instructions
+    'Welcome to "Split Bill Easy"!': 'Welcome to "Split Bill Easy"!',
+    'Say goodbye to the hassle of dividing restaurant bills! Our app makes it simple and quick:':
+      'Say goodbye to the hassle of dividing restaurant bills! Our app makes it simple and quick:',
+    '1. Payers Tab': '1. Payers Tab',
+    "Add everyone who's sharing the bill.": "Add everyone who's sharing the bill.",
+    '2. Items Tab': '2. Items Tab',
+    'Enter each item on the bill, who ordered it, and how the cost is split.':
+      'Enter each item on the bill, who ordered it, and how the cost is split.',
+    '3. Calculations Tab': '3. Calculations Tab',
+    'Add any extra charges like service fees or tips.': 'Add any extra charges like service fees or tips.',
+    'Then, just copy and share the final breakdown with your friends. Enjoy your meal without the awkward math! 🍽️👍':
+      'Then, just copy and share the final breakdown with your friends. Enjoy your meal without the awkward math! 🍽️👍',
+  },
+};
+
+export { enTranslations };
